1ebf8307ccb985297ecc5c02980cfbf49712cbf0
Tiana <97kth01@naver.com>
2022-05-04 14:16:24 +0900

    feat: 모델 프레임워크 추가, Main 스토리보드 삭제, 그룹 나누기

---
+++ BankManagerUIApp/BankManagerUIApp/BankController/BankView.swift
@@ -0,0 +1,10 @@
+//
+//  BankView.swift
+//  BankManagerUIApp
+//
+//  Created by 김태현 on 2022/05/04.
+//
+
+import UIKit
+
+class BankView: UIView { }
--- BankManagerUIApp/BankManagerUIApp/BankController/BankViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/BankController/BankViewController.swift
@@ -6,13 +6,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class BankViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
